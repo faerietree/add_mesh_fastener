cb5a10d008b9ebf93b2c731fa2d199655b4a55b1
Jan R.I. B.F. v.W. v.Z. (faerietree) <radagast@ciry.at>
2017-01-20 07:23:39 +0100

    Update metric thread standard lengths to follow ISO 888.
    Also remove two redundant shank diameter definitions in M12-*.py.
    
    => Reduces parameters to maintain by 15 + 2 = 17.

--- presets/M20.py
+++ presets/M20.py
@@ -1,21 +1,26 @@
-props.bf_Shank_Dia = 20.0
+# Thread
 props.bf_Pitch = 2.5 # Coarse
 props.bf_Crest_Percent = 10
 props.bf_Root_Percent = 10
 props.bf_Major_Dia = 20.0
 props.bf_Minor_Dia = props.bf_Major_Dia - (1.082532 * props.bf_Pitch)
+# Head
 props.bf_Hex_Head_Flat_Distance = 30.0
 props.bf_Hex_Head_Height = 12.5
 props.bf_Cap_Head_Dia = 30.0
 props.bf_Cap_Head_Height = 19.5
 props.bf_CounterSink_Head_Dia = 36.0
-props.bf_Allen_Bit_Flat_Distance = 17.0
-props.bf_Allen_Bit_Depth =  10.2  # .6 * Bit Flat Distance
 props.bf_Pan_Head_Dia = 36.0
 props.bf_Dome_Head_Dia = 36.0
+# Bit
+props.bf_Allen_Bit_Flat_Distance = 17.0
+props.bf_Allen_Bit_Depth =  10.2  # .6 * Bit Flat Distance
 props.bf_Philips_Bit_Dia = props.bf_Pan_Head_Dia * (1.82 / 5.6)
-#props.bf_Phillips_Bit_Depth = Get_Phillips_Bit_Height(props.bf_Philips_Bit_Dia)
+# Nut
 props.bf_Hex_Nut_Height = 14.9
 props.bf_Hex_Nut_Flat_Distance = 30.0
-props.bf_Thread_Length = 46.0
-props.bf_Shank_Length = 125.0 - props.bf_Thread_Length # TODO Overall length - Thread Length
+# Shank
+props.bf_Shank_Dia = 20.0
+props.bf_Length = 55
+#props.bf_Thread_Length =
+#props.bf_Shank_Length =
