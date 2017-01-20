91708741bacba1754841667463725708ed727e7a
Jan R.I. B.F. v.W. v.Z. (faerietree) <radagast@ciry.at>
2017-01-20 09:06:07 +0100

    Format previously forgotten M03-0.35.py like the other presets.

--- presets/M03-0.35.py
+++ presets/M03-0.35.py
@@ -1,21 +1,26 @@
-props.bf_Shank_Dia = 3.0
+# Thread
 props.bf_Pitch = 0.35   # Fine
 props.bf_Crest_Percent = 10
 props.bf_Root_Percent = 10
 props.bf_Major_Dia = 3.0
 props.bf_Minor_Dia = props.bf_Major_Dia - (1.082532 * props.bf_Pitch)
+# Head
 props.bf_Hex_Head_Flat_Distance = 5.5
 props.bf_Hex_Head_Height = 2.0
 props.bf_Cap_Head_Dia = 5.5
 props.bf_Cap_Head_Height = 3.0
 props.bf_CounterSink_Head_Dia = 6.3
-props.bf_Allen_Bit_Flat_Distance = 2.5
-props.bf_Allen_Bit_Depth = 1.5
 props.bf_Pan_Head_Dia = 5.6
 props.bf_Dome_Head_Dia = 5.6
+# Bit
+props.bf_Allen_Bit_Flat_Distance = 2.5
+props.bf_Allen_Bit_Depth = 1.5
 props.bf_Philips_Bit_Dia = props.bf_Pan_Head_Dia * (1.82 / 5.6)
-#props.bf_Phillips_Bit_Depth = Get_Phillips_Bit_Height(props.bf_Philips_Bit_Dia)
+# Nut
 props.bf_Hex_Nut_Height = 2.4
 props.bf_Hex_Nut_Flat_Distance = 5.5
+# Shank
+props.bf_Shank_Dia = 3.0
+#props.bf_Length = 6
 props.bf_Thread_Length = 6
 props.bf_Shank_Length = 0.0
